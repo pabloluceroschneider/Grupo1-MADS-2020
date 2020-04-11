b53515ff3d7986621e026f2ee4c7df10cc4d818c
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-04-10 22:52:20 -0300

    init: Creo carpeta de server.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect( ()=>{
+    fetch('/hello').then( res => {
+      res.json().then(data => {
+        console.log(data);
+      })
+    })
+  },[]);
+
   return (
     <div className="App">
       <header className="App-header">
