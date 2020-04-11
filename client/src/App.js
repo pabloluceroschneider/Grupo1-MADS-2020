298b02c01f6cd4898d600e1acd503166cf4a6047
BrunoConti <bruno.conti1997@gmail.com>
2020-04-10 22:58:21 -0300

    Agrego comentarios

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//Pagina inicio...
+
 function App() {
   useEffect( ()=>{
     fetch('/hello').then( res => {
