c87094c5c44b8230cffa6d2b399af82f2a3b4571
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-04-12 22:26:29 -0300

    step: En estos archivos se crearán los modelos/ clases, principalmente el de Publicacion.

---
+++ server/api/v1/models/publicaciones.py
@@ -0,0 +1,8 @@
+# class Publicacion():
+#     def __init__(self, id, location, roomApartment, price, owner, datePublish):
+#         self.id = id
+#         self.location = location
+#         self.roomApartment = roomApartment
+#         self.price = price
+#         self.owner = owner
+#         self.datePublish = datePublish  
