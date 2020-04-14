85b4b27b10faa877160f2c24ff6c5fac7253b424
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-04-14 16:44:09 -0300

    feat: Llevo modelo a models/Publicacion

--- server/api/v1/controllers/PublicacionCtrl.py
+++ server/api/v1/controllers/PublicacionCtrl.py
@@ -1,25 +1,5 @@
 from flask import Flask, jsonify
-import json
-
-class Publicacion:
-    def __init__(self, id, location, roomApartment, price, owner, datePublished):
-        self.id : int = id
-        self.location : str = location
-        self.roomApartment : str = roomApartment
-        self.price : float = price
-        self.owner : str = owner
-        self.datePublished : str = datePublished
-    
-    def tojson(self):
-        json = { 
-            "id" : self.id, 
-            "location" : self.location,
-            "roomApartment":  self.roomApartment,
-            "price" : self.price,
-            "owner": self.owner,
-            "datePublished": self.datePublished
-        } 
-        return json
+from models import Publicacion
 
 def get(mysql):
         cur = mysql.connection.cursor()
@@ -28,6 +8,6 @@ def get(mysql):
         response = []
         for register in data:
             id, location, roomApartment, price, owner, datePublished = register
-            obj = Publicacion(id, location, roomApartment, price, owner, datePublished).tojson()
+            obj = Publicacion.Publicacion(id, location, roomApartment, price, owner, datePublished).tojson()
             response.append(obj)
         return jsonify({ "data" : response })
