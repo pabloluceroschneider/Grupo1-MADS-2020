66085c6bf5bdc058e0b96f71d2aa9a9c71c61fcf
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-04-14 00:01:00 -0300

    step: Modifico el retorno de las publicaciones, utilizando la clase y el motodo tojson()

--- server/api/v1/routes/routes.py
+++ server/api/v1/routes/routes.py
@@ -1,4 +1,26 @@
 from flask import Flask, jsonify
+import json
+
+class Publicacion:
+    def __init__(self, data):
+        self.id = data[0]
+        self.location = data[1]
+        self.roomApartment = data[2]
+        self.price = data[3]
+        self.owner = data[4]
+        self.datePublished = data[5]
+    
+    def tojson(self):
+        json = { 
+            "id" : self.id, 
+            "location" : self.location,
+            "roomApartment":  self.roomApartment,
+            "price" : self.price,
+            "owner": self.owner,
+            "datePublished": self.datePublished
+        } 
+        return json
+    
 
 def routes(app, mysql):    
     #INDEX
@@ -11,4 +33,8 @@ def routes(app, mysql):
         cur = mysql.connection.cursor()
         cur.execute('CALL LISTAR_PUBLICACIONES')
         data = cur.fetchall()
-        return jsonify({"data":data})
+        response = []
+        for register in data:
+            obj = Publicacion(register).tojson()
+            response.append(obj)
+        return jsonify({ "data" : response })
