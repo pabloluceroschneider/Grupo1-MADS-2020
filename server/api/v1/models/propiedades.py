ba33f1ebf6ed88886853a03a76794d93be535e6a
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-04-14 16:43:17 -0300

    delete: Elimino modelos

--- server/api/v1/models/propiedades.py
+++
@@ -1,7 +0,0 @@
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     location = db.Column(db.String(50), nullable=False)
-#     roomApartment = db.Column(db.Integer, nullable=False)
-#     price = db.Column(db.Integer, nullable=False)
-#     owner = db.Column(db.String(50), nullable=False)
-#     datePublish = db.Column(db.Date, nullable=False)
--- server/api/v1/models/publicaciones.py
+++
@@ -1,8 +0,0 @@
-# class Publicacion():
-#     def __init__(self, id, location, roomApartment, price, owner, datePublish):
-#         self.id = id
-#         self.location = location
-#         self.roomApartment = roomApartment
-#         self.price = price
-#         self.owner = owner
-#         self.datePublish = datePublish  
