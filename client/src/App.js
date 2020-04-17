d6ec7497e463144212f8ff7979a93b9b7fefe740
agustinperalta <43344932+agustinperalta@users.noreply.github.com>
2020-04-17 18:48:22 -0300

    step: Llamado al Server con fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import React, {useEffect} from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import { ListOwnership } from './Components/ListOwnership'
 import {Button} from 'semantic-ui-react'
 //Pagina inicio...
 
-function App() {
-  useEffect( ()=>{
-    fetch('/publicaciones').then( res => {
-      res.json().then(data => {
-        console.log(data);
-      })
-    })
-  },[]);
-
+class App extends Component {
+ 
+ render(){
   return (
     <div className="App">
         <ListOwnership/>
     </div>
   );
 }
+}
+
+ 
 
 export default App;
--- client/src/Components/ListOwnership.js
+++ client/src/Components/ListOwnership.js
@@ -1,31 +1,23 @@
 import React, {Component} from 'react'
-import properties from '../Data/Ownerships.json'
+
 
 export class ListOwnership extends Component{
-    // state={ownship: []}
+ state={ownship: []}
 
 
-  //  componentDidMount(){
-  //  fetch('./Data/Ownerships.json').
-  //  then(res => res.json).
-  //      then(data => {
-  //          console.log(data)
-  //      })
-  //  }
+   componentDidMount(){
+    fetch('/publicaciones')
+        .then(res => res.json())
+            .then(data => {
+           console.log(data)
+       })
+   }
 
 
     render(){
         return (
             <div>
                 Listado de Propiedades
-                {console.log(properties)}
-
-                {properties.map(
-                    prop =>{
-                        return(
-                            <h1 key={prop.id}>{prop.id}</h1>)
-                        }
-                        )}
             </div>
         )
     }
