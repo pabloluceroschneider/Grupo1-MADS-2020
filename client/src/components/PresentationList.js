0a467f245dc15893076c6bbcde1f88cc8926c94b
BrunoConti <bruno.conti1997@gmail.com>
2020-04-18 15:09:43 -0300

    fixed: error al querer ingresar a opciones de busq

--- client/src/components/PresentationList.js
+++ client/src/components/PresentationList.js
@@ -7,13 +7,13 @@ export class PresentationList extends Component {
         return (
             <div className='PresentationalList'>
                 <div className='CardPresentacionDer'>
-                <CardPresentacion  title='Buscar Compañero' description='Encuentra al compañero perfecto para compartir propiedad' imagen='https://www.cheatsheet.com/wp-content/uploads/2019/05/Joey-and-Chandler.jpg'></CardPresentacion>
+                <CardPresentacion handlerPage={this.props.handlerPage} pagina='' title='Buscar Compañero' description='Encuentra al compañero perfecto para compartir propiedad' imagen='https://www.cheatsheet.com/wp-content/uploads/2019/05/Joey-and-Chandler.jpg'></CardPresentacion>
                 </div>
                 <div className='CardPresentacionIzq'>
-                <CardPresentacion  title='Publicar Propiedad' description='Publica tu propiedad' imagen='https://www.hogares.cl/wp-content/uploads/2018/06/SLA_3734.jpg'></CardPresentacion>
+                <CardPresentacion handlerPage={this.props.handlerPage} pagina='' title='Publicar Propiedad' description='Publica tu propiedad' imagen='https://www.hogares.cl/wp-content/uploads/2018/06/SLA_3734.jpg'></CardPresentacion>
                 </div>
                 <div className='CardPresentacionDer'>
-                <CardPresentacion  title='Buscar Compañero y Propiedad' description='Busca compañeros y propiedades para compartir' imagen='https://blog.nexoinmobiliario.pe/wp-content/uploads/2019/05/Roommates.jpg'></CardPresentacion>
+                <CardPresentacion handlerPage={this.props.handlerPage} pagina='' title='Buscar Compañero y Propiedad' description='Busca compañeros y propiedades para compartir' imagen='https://blog.nexoinmobiliario.pe/wp-content/uploads/2019/05/Roommates.jpg'></CardPresentacion>
                 </div>
                 <div className='CardPresentacionIzq'>
                 <CardPresentacion handlerPage={this.props.handlerPage} pagina='ListaPropiedades' title='Buscar Propiedad' description='Busca la propiedad que mejor se adapte a ti' imagen='https://imgar.zonapropcdn.com/avisos/1/00/45/59/55/84/720x532/1716688221.jpg'></CardPresentacion>
