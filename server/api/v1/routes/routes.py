4152dcb2e60abc1b8c9eda137e34cfbbf3fc2f09
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-05-02 16:50:24 -0300

    step: Primera invocación desde api al nuevo procedimiento.

--- server/api/v1/routes/routes.py
+++ server/api/v1/routes/routes.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from controllers import PublicacionCtrl
 
 def routes(app, mysql):    
@@ -9,3 +10,10 @@ def routes(app, mysql):
     @app.route('/publicaciones')
     def get():
         return PublicacionCtrl.get(mysql)
+    
+    @app.route('/propiedades')
+    def getPropiedades():
+        cur = mysql.connection.cursor()
+        cur.execute('CALL LISTAR_PROPIEDADES')
+        data = cur.fetchall()
+        return jsonify(data)
