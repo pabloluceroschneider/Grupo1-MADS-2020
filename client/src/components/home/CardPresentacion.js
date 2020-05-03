d949f86996165b8f72edf4a352a5306e6ebab4db
BrunoConti <bruno.conti1997@gmail.com>
2020-05-03 19:45:29 -0300

    delete: field descripcion not used

--- client/src/components/home/CardPresentacion.js
+++ client/src/components/home/CardPresentacion.js
@@ -4,13 +4,12 @@ import PropTypes from "prop-types";
 export class CardPresentacion extends Component {
   static propTypes = {
     title: PropTypes.string,
-    description: PropTypes.string,
     imagen: PropTypes.string,
     pagina: PropTypes.string,
   };
 
   render() {
-    const { title, description, imagen, pagina, handlerPage } = this.props;
+    const { title, imagen, pagina, handlerPage } = this.props;
 
     return (
       <div className="ui card">
