08f7454b16c6a303be222551cff4abed19938202
BrunoConti <bruno.conti1997@gmail.com>
2020-05-02 23:46:46 -0300

    step: Update metodo getPropiedades()

--- server/api/v1/routes/routes.py
+++ server/api/v1/routes/routes.py
@@ -52,51 +52,19 @@ def routes(app, mysql):
         cur.execute('CALL LISTAR_PROPIEDADES')
         data = cur.fetchall()
         response = []
-        rows = list(filter(lambda x: x[0]==1,data))
-        id, ubicacion, habitaciones, fechaPublicacion, lat, longitud, amenities, value, imagenes = rows[0]
-        obj = Propiedad(id, ubicacion, habitaciones, fechaPublicacion, lat, longitud, [], []).tojson()
-        for register in rows:
-            amenities = {"descripcion":register[6],"valor":register[7]}
-            imagen = register[8]
-            if amenities not in obj["amenities"]:
-                obj['amenities'].append(amenities)
-            if imagen not in obj["imagenes"]:
-                obj['imagenes'].append(imagen)
-            # obj['imagenes'].append(imagen)
-
-
-        # for register in data:
-        #     id, ubicacion, habitaciones, fechaPublicacion, lat, longitud, amenities, value, imagenes = register
-        #     obj = Propiedad(id, ubicacion, habitaciones, fechaPublicacion, lat, longitud, amenities, value, imagenes).tojson()
-        #     response.append(obj)
-
-        # for i in response:
-        #     print(i['amenities'])
-
-        return jsonify(obj)
-
-    # @app.route('/bizarra')
-    # def getBizarra():
-    #     cur = mysql.connection.cursor()
-    #     cur.execute('SELECT * FROM PROPIEDADES')
-    #     data = cur.fetchall()
-    #     reg = []
-    #     for register in data:
-    #         id, ubicacion, habitaciones, fechaPublicacion, lat, longitud, usuario = register
-    #         json = { 
-    #             "id" : id, 
-    #             "ubicacion" : ubicacion,
-    #             "habitaciones":  habitaciones,
-    #             "fechaPublicacion" : fechaPublicacion,
-    #             "lat": lat,
-    #             "longitud": longitud,
-    #             "amenities": [],
-    #             "imagenes": [],
-    #         }
-    #         cur = mysql.connection.cursor()
-    #         cur.execute('SELECT * FROM PROPIEDADESXAMENITIES WHERE IDPROPIEDADES = 1')
-    #         data = cur.fetchall()
-    #         print(data)
-
-    #         reg.append(json)
-    #     return jsonify(reg)
+        arrayId = []
+        for row in data:
+            rows = list(filter(lambda x: x[0]==row[0],data))
+            id, ubicacion, habitaciones, fechaPublicacion, lat, longitud, amenities, value, imagenes = rows[0]
+            if id not in arrayId:
+                arrayId.append(id)
+                obj = Propiedad(id, ubicacion, habitaciones, fechaPublicacion, lat, longitud, [], []).tojson()
+                for register in rows:
+                    amenities = {"descripcion":register[6],"valor":register[7]}
+                    imagen = register[8]
+                    if amenities not in obj["amenities"]:
+                        obj['amenities'].append(amenities)
+                    if imagen not in obj["imagenes"]:
+                        obj['imagenes'].append(imagen)
+                response.append(obj)
+        return jsonify(response)
