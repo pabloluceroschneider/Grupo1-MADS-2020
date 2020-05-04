bbb3af532713822f17fa15b698d7db1f24733625
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-05-04 00:37:06 -0300

    step: Funcionalidad Front de input y preparación de requestBody

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,31 +1,52 @@
 import React, { useState } from "react";
 import { Form, Button } from "semantic-ui-react";
-
 // use Filter.css
 
 const InputRadio = props => {
-  const {label, value} = props
+  const {label, id, value, setRequestBody, requestBody } = props
+
+  const handleCheck = () => {
+    if ( id === "todas" ){
+      if (requestBody.wifi === 1){
+        setRequestBody({...requestBody, wifi: null, balcon:null, ascensor:null, cochera:null, asador:null, patio:null })
+      }else{
+        setRequestBody({...requestBody, wifi: 1, balcon:1, ascensor:1, cochera:1, asador:1, patio:1 })
+      }
+    }else{
+      if (requestBody[id] === value){
+        setRequestBody({...requestBody, [id]: null })
+      }else{
+        setRequestBody({...requestBody, [id]: value })
+      }
+    }
+  }
+
   return (
     <div>
       <Form.Radio
         label={label}
-        value={value}
-        checked={value}
-        // onChange={ () => handleChange(parent, asc)}
+        checked={ requestBody[id] === value }
+        onClick={ () => handleCheck() }
       />
     </div>
   );
 };
 
 const Filtro = props => {
-  const {filter, index} = props;
+  const { filter, index, setRequestBody, requestBody } = props;
   return (
     <div key={index} className="grid-item">
       <label className="filterName">{filter.title}</label>
-      {filter.filters.map( f => {
+      {filter.filters.map( (f,index) => {
         return (
-          <div className="filter">
-            <InputRadio label={f.label} value={f.value}/>
+          <div key={index} className="filter">
+            <InputRadio 
+              label={f.label} 
+              id={f.id} 
+              value={f.value} 
+              setRequestBody={setRequestBody}
+              requestBody={requestBody}
+              />
           </div>
         )
       })}
@@ -39,37 +60,55 @@ const PanelFiltros = () => {
     {
       "title": "Tipo de Contrato",
       "filters":[
-        {"label":"Dueño Directo", "value":true},
-        {"label":"Inmobiliaria", "value":false},
-        {"label":"Dueño/Inmobiliaria", "value":false}
-      ]
-    },{
-      "title": "Comodidades",
-      "filters":[
-        {"label":"Wifi", "value":true},
-        {"label":"Balcon", "value":true},
-        {"label":"Ascensor", "value":true},
-        {"label":"Cochera", "value":false},
-        {"label":"Asador", "value":false},
-        {"label":"Patio", "value":false},
+        { "label":"Dueño Directo", "id":"contrato", "value": "propietario" },
+        { "label":"Inmobiliaria", "id":"contrato", "value": "inmobiliaria" },
+        { "label":"Dueño/Inmobiliaria", "id":"contrato", "value": "duenoinmobiliaria" }
       ]
     },{
       "title": "Cantidad de habitaciones",
       "filters":[
-        {"label":"1", "value":true},
-        {"label":"2", "value":false},
-        {"label":"3", "value":false},
+        { "label":"1","id":"habitaciones", "value":1 },
+        { "label":"2", "id":"habitaciones", "value":2 },
+        { "label":"3", "id":"habitaciones", "value":3 },
       ]
-    }]
+    },{
+      "title": "Comodidades",
+      "filters":[
+        { "label":"Wifi", "id":"wifi", "value":1 },
+        { "label":"Balcon","id":"balcon", "value":1 },
+        { "label":"Ascensor","id":"ascensor", "value":1 },
+        { "label":"Cochera","id":"cochera", "value":1 },
+        { "label":"Asador","id":"asador", "value":1 },
+        { "label":"Patio","id":"patio", "value":1 },
+        { "label":"Todas","id":"todas", "value":1 },
+      ]
+    }
+  ]
 
+  const [ requestBody, setRequestBody ] = useState({
+    "habitaciones": null,
+    "contrato": null,
+    "wifi": null,
+    "ascensor": null,
+    "cochera": null,
+    "asador": null,
+    "patio": null
+  })
 
   return (
     <>
+      {console.log(requestBody)}
       <Form.Group inline>
         <div className="panelFilter">
-          {filtros.map(( filter, index) => {
+          {filtros.map( (filter, index) => {
             return (
-              <Filtro filter={filter} index={index} key={index} />              
+              <Filtro 
+                key={index} 
+                filter={filter} 
+                index={index} 
+                setRequestBody={setRequestBody}
+                requestBody={requestBody}
+                />              
             )
           })}
         </div>
