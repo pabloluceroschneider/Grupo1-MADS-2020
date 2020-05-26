28611495cdc9c85305c51700b7ff61363f0ec14c
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-05-25 22:53:58 -0300

    add: Test mock

---
+++ client/src/__mock__/filterpropiedades/index.js
@@ -0,0 +1,12 @@
+export default function getFiltrosPropiedades() {
+	return {
+		habitaciones: 1,
+		contrato: null,
+		wifi: null,
+		ascensor: null,
+		cochera: null,
+		asador: null,
+		patio: null,
+		allAmenities: null
+	};
+}
--- client/src/__test__/index.test.js
+++ client/src/__test__/index.test.js
@@ -1,5 +1,12 @@
 const sum = require('./sum/sum');
+const propiedades = require('../__mock__/propiedades');
 
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test('propiedades con una habitacion', () =>{
+  var prop = propiedades.find( p => p.habitaciones === 1);
+  var hab = prop.habitaciones;
+  expect(hab).toBe(1);
 });
