a8fc647a8eddbf1a5f014b9fb39a0de7dd6965f0
Pablo Lucero Schneider <pabloluceroschneider@gmail.com>
2020-06-25 01:13:41 -0300

    Guarda muchas fotos

--- client/src/components/FormCreateProperty/index.js
+++ client/src/components/FormCreateProperty/index.js
@@ -9,112 +9,194 @@ const options = [
 ];
 
 const FormProperty = () => {
-    const firstInput = useRef(null);
-    const [fields, setFields] = useState({
-        habitaciones : null,
-    	fechaPublicacion: new Date(),
-	    lat: null,
-	    longitud: null,
-	    ubicacion: null,
-	    usuario: {
-            descripcion: null,
-            rol: 1
-        },
-        amenities: {
-            Asador: null,
-            Cochera: null,
-            Wifi: null,
-            Patio: null,
-            Balcon: null,
-            Ascensor: null
-        },
+	const firstInput = useRef(null);
+	const [ fields, setFields ] = useState({
+		habitaciones: null,
+		fechaPublicacion: new Date(),
+		lat: null,
+		longitud: null,
+		ubicacion: null,
+		usuario: {
+			descripcion: null,
+			rol: 1
+		},
+		amenities: {
+			Asador: null,
+			Cochera: null,
+			Wifi: null,
+			Patio: null,
+			Balcon: null,
+			Ascensor: null
+		},
 		precios: null,
 		images: null
-    })
+	});
 
 	useEffect(() => {
 		firstInput.current.focus();
 	}, []);
-	
-	const saveImages = async files => {
-		let bucketName = "images"
-		let file = files[0];
-		let storageRef = firebase.storage().ref(`${bucketName}/${file.name}`)
-		let uploadTask = storageRef.put(file)
-		uploadTask.on(
-			firebase.storage.TaskEvent.STATE_CHANGED, 
-			() => {
-				let downloadURL = uploadTask.snapshot.downloadURL
-			})
-	}
-    
-    const onSubmitProperty = async () => {
-        let postJson = {...fields, amenities:[]}
-        postJson.amenities.push( {descripcion: "Asador", valor: fields.amenities.Asador})
-        postJson.amenities.push( {descripcion: "Cochera", valor: fields.amenities.Cochera})
-        postJson.amenities.push( {descripcion: "Wifi", valor: fields.amenities.Wifi})
-        postJson.amenities.push( {descripcion: "Patio", valor: fields.amenities.Patio})
-        postJson.amenities.push( {descripcion: "Balcon", valor: fields.amenities.Balcon})
-        postJson.amenities.push( {descripcion: "Ascensor", valor: fields.amenities.Ascensor})
 
-		
-		postJson = {...postJson, precios:[]}
-        postJson.precios.push( {idPrecio: 1, descripcion: "Total", monto: fields.precios})
-	
-		await saveImages(postJson.images)
-		
-		let name = postJson.images[0].name
-		postJson = { ...postJson, images: [name] }
+	const saveImages = async (files) => {
+		let bucketName = 'images';
 
-		console.log("postJson -->", postJson)
+		Array.from(files).forEach(file => { 
+			let storageRef = firebase.storage().ref(`${bucketName}/${file.name}`);
+			let uploadTask = storageRef.put(file);
+			uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, () => {
+				let downloadURL = uploadTask.snapshot.downloadURL;
+			});
+		 });
+	};
 
-        await post("/property", postJson)
-    }
+	const onSubmitProperty = async () => {
+		let postJson = { ...fields, amenities: [] };
+		postJson.amenities.push({ descripcion: 'Asador', valor: fields.amenities.Asador });
+		postJson.amenities.push({ descripcion: 'Cochera', valor: fields.amenities.Cochera });
+		postJson.amenities.push({ descripcion: 'Wifi', valor: fields.amenities.Wifi });
+		postJson.amenities.push({ descripcion: 'Patio', valor: fields.amenities.Patio });
+		postJson.amenities.push({ descripcion: 'Balcon', valor: fields.amenities.Balcon });
+		postJson.amenities.push({ descripcion: 'Ascensor', valor: fields.amenities.Ascensor });
+
+		postJson = { ...postJson, precios: [] };
+		postJson.precios.push({ idPrecio: 1, descripcion: 'Total', monto: fields.precios });
+
+		await saveImages(postJson.images);
+
+		let names = [];
+		Array.from(postJson.images).forEach( file => {
+			names.push( file.name )
+		})
+
+		postJson = { ...postJson, images: names };
+
+		console.log('postJson -->', postJson);
+
+		await post('/property', postJson);
+	};
 
 	return (
 		<div className="form-property">
 			<Form>
 				<Form.Field>
 					<label>Direccion</label>
-					<input ref={firstInput} placeholder="Direccion" onChange={ event => setFields({...fields, ubicacion: event.target.value})  } />
+					<input
+						ref={firstInput}
+						placeholder="Direccion"
+						onChange={(event) => setFields({ ...fields, ubicacion: event.target.value })}
+					/>
 				</Form.Field>
 
 				<Form.Field>
-					<Form.Input type="number" fluid label="Habitaciones" placeholder="Habitaciones" onChange={ event => setFields({...fields, habitaciones: parseInt(event.target.value)})} />
+					<Form.Input
+						type="number"
+						fluid
+						label="Habitaciones"
+						placeholder="Habitaciones"
+						onChange={(event) => setFields({ ...fields, habitaciones: parseInt(event.target.value) })}
+					/>
 				</Form.Field>
 
 				<Form.Group widths="equal">
-					<Form.Input fluid label="Latitud" placeholder="Latitud" onChange={ event => setFields({...fields, lat: event.target.value})}  />
-					<Form.Input fluid label="Longitud" placeholder="Longitud" onChange={ event => setFields({...fields, longitud: event.target.value})}  />
+					<Form.Input
+						fluid
+						label="Latitud"
+						placeholder="Latitud"
+						onChange={(event) => setFields({ ...fields, lat: event.target.value })}
+					/>
+					<Form.Input
+						fluid
+						label="Longitud"
+						placeholder="Longitud"
+						onChange={(event) => setFields({ ...fields, longitud: event.target.value })}
+					/>
 				</Form.Group>
 
 				<Form.Group grouped style={{ textAlign: 'left', marginLeft: '50px' }}>
 					<label>Comodidades</label>
-					<Form.Field label="Asador" control="input" type="checkbox" onChange={ () => setFields({...fields, amenities: {...fields.amenities, Asador: !fields.amenities.Asador} })} />
-					<Form.Field label="Cochera" control="input" type="checkbox" onChange={ () => setFields({...fields, amenities: {...fields.amenities, Cochera: !fields.amenities.Cochera} })} />
-					<Form.Field label="Wifi" control="input" type="checkbox" onChange={ () => setFields({...fields, amenities: {...fields.amenities, Wifi: !fields.amenities.Wifi} })}/>
-					<Form.Field label="Patio" control="input" type="checkbox" onChange={ () => setFields({...fields, amenities: {...fields.amenities, Patio: !fields.amenities.Patio} })}/>
-					<Form.Field label="Balcon" control="input" type="checkbox" onChange={ () => setFields({...fields, amenities: {...fields.amenities, Balcon: !fields.amenities.Balcon} })}/>
-					<Form.Field label="Ascensor" control="input" type="checkbox" onChange={ () => setFields({...fields, amenities: {...fields.amenities, Ascensor: !fields.amenities.Ascensor} })}/>
+					<Form.Field
+						label="Asador"
+						control="input"
+						type="checkbox"
+						onChange={() =>
+							setFields({
+								...fields,
+								amenities: { ...fields.amenities, Asador: !fields.amenities.Asador }
+							})}
+					/>
+					<Form.Field
+						label="Cochera"
+						control="input"
+						type="checkbox"
+						onChange={() =>
+							setFields({
+								...fields,
+								amenities: { ...fields.amenities, Cochera: !fields.amenities.Cochera }
+							})}
+					/>
+					<Form.Field
+						label="Wifi"
+						control="input"
+						type="checkbox"
+						onChange={() =>
+							setFields({ ...fields, amenities: { ...fields.amenities, Wifi: !fields.amenities.Wifi } })}
+					/>
+					<Form.Field
+						label="Patio"
+						control="input"
+						type="checkbox"
+						onChange={() =>
+							setFields({
+								...fields,
+								amenities: { ...fields.amenities, Patio: !fields.amenities.Patio }
+							})}
+					/>
+					<Form.Field
+						label="Balcon"
+						control="input"
+						type="checkbox"
+						onChange={() =>
+							setFields({
+								...fields,
+								amenities: { ...fields.amenities, Balcon: !fields.amenities.Balcon }
+							})}
+					/>
+					<Form.Field
+						label="Ascensor"
+						control="input"
+						type="checkbox"
+						onChange={() =>
+							setFields({
+								...fields,
+								amenities: { ...fields.amenities, Ascensor: !fields.amenities.Ascensor }
+							})}
+					/>
 				</Form.Group>
 
 				<Form.Group widths="equal">
 					<Form.Select
-                        name="select"
+						name="select"
 						label="Tipo de contrato"
 						options={options}
-                        placeholder="Seleccione tipo de contrato..."
-                        onChange={ (e, { name, value }) => setFields({...fields, usuario: {...fields.usuario, descripcion: value}})}
+						placeholder="Seleccione tipo de contrato..."
+						onChange={(e, { name, value }) =>
+							setFields({ ...fields, usuario: { ...fields.usuario, descripcion: value } })}
 					/>
-					<Form.Input type="number" fluid label="Precio" placeholder="Precio" onChange={ event => setFields({...fields, precio: event.target.value})}  />
+					<Form.Input
+						type="number"
+						fluid
+						label="Precio"
+						placeholder="Precio"
+						onChange={(event) => setFields({ ...fields, precio: event.target.value })}
+					/>
 				</Form.Group>
 
 				<Form.Field>
-					<input type="file" multiple onChange={ e => setFields( { ...fields, images: e.target.files })  } />
+					<input type="file" multiple onChange={(e) => setFields({ ...fields, images: e.target.files })} />
 				</Form.Field>
 
-                <Button type='submit' onClick={onSubmitProperty}>Publicar</Button>
-	
+				<Button type="submit" onClick={onSubmitProperty}>
+					Publicar
+				</Button>
 			</Form>
 		</div>
 	);
