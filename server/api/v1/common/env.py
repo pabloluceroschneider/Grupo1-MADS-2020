739cfa790541d9cf1ec4222e7439025962f1f7be
BrunoConti <bruno.conti1997@gmail.com>
2020-06-25 20:28:04 -0300

    Delete: python backend

--- server/api/v1/common/env.py
+++
@@ -1,7 +0,0 @@
-
-def config(app):
-    app.config['MYSQL_HOST']        = 'localhost'
-    app.config['MYSQL_USER']        = 'root'
-    app.config['MYSQL_PASSWORD']    = 'root'
-    app.config['MYSQL_DB']          = 'mads'
-    return app
